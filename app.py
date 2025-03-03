454d42bfb07abcb6263ddda57ca5ac4f4193aafd
Black <blackbeast4545@gmail.com>
2025-03-03 06:39:51 +0530

    Update application to use netwatch templates

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from netwatch import create_app, socketio
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
-app = create_app()
+app = Flask(__name__, 
+           template_folder='netwatch/templates',
+           static_folder='netwatch/static')
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
-if __name__ == "__main__":
-    socketio.run(app) 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True) 
